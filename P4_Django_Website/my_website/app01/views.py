0459f6c5c1e049557bf019e260ca940d7c83600b
suliuer <suliux@qq.com>
2017-09-22 02:58:00 +0800

    Django 轮播图。
    Django 轮播图。

---
+++ P4_Django_Website/my_website/app01/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from app01 import models
+
+# Create your views here.
+
+
+def index(request):
+    # models.BxSlider.objects.filter(status=1)  # 对象
+    queryset_dict = models.BxSlider.objects.filter(status=1).values('img', 'href', 'name')
+    # queryset_list = models.BxSlider.objects.filter(status=1).values_list('img', 'href', 'name')
+    return render(request, 'index.html', {'queryset_dict': queryset_dict})
