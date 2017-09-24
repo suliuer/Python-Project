cfee5c6e95dfcec94675b584c63a09eef872a7a8
suliuer <suliux@qq.com>
2017-09-24 22:52:32 +0800

    组合搜索。
    组合搜索。

---
+++ P4_Django_MultiSearch/MultiSearch/multi_search/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MultiSearchConfig(AppConfig):
+    name = 'multi_search'
--- P4_Django_Website/my_website/app01/views.py
+++ P4_Django_Website/my_website/app01/views.py
@@ -9,3 +9,8 @@ def index(request):
     queryset_dict = models.BxSlider.objects.filter(status=1).values('img', 'href', 'name')
     # queryset_list = models.BxSlider.objects.filter(status=1).values_list('img', 'href', 'name')
     return render(request, 'index.html', {'queryset_dict': queryset_dict})
+
+
+def students(request):
+
+    return render(request, 'student.html')
